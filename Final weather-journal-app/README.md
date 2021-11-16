e71dcf2c692a828633267635123bee71be05184a
HossamGouda <hos.gouda90@gmail.com>
2021-11-16 13:58:29 +0300

    updates on app.js file for Async Function and Update user UI

--- Final weather-journal-app/README.md
+++ Final weather-journal-app/README.md
@@ -8,3 +8,22 @@ This will require modifying the `server.js` file and the `website/app.js` file. 
 
 ## Extras
 If you are interested in testing your code as you go, you can use `tests.js` as a template for writing and running some basic tests for your code.
+
+
+Steps: 
+1-created local server with express package 
+2-installed and required the requested packages as (express, cors,body-parser)
+3-I replaced body-parser with express since it is became intgrated with express.
+4-created server and runing it in port locally then creating the routs (get,post)
+5-added a meta tag in th html file to make it responsive.
+
+
+resources :
+https://www.w3schools.com/
+https://www.youtube.com/c/ElzeroInfo/community  (some videos from this youtube Channale)
+https://developer.mozilla.org/en-US/
+https://dev.to/ramonak/javascript-how-to-access-the-return-value-of-a-promise-object-1bck
+FWD community
+https://www.stechies.com/uncaught-syntaxerror-unexpected-end-json-input/
+https://bithacker.dev/fetch-weather-openweathermap-api-javascript
+
--- Final weather-journal-app/website/app.js
+++ Final weather-journal-app/website/app.js
@@ -7,41 +7,41 @@ const key = "00fc2967f7b4d95b93d15e5d43e64586";
 const btn = document.querySelector('#generate');
 
 
-btn.addEventListener('click', action);
-async function action() {
-    const zip = document.querySelector('#zip').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${key}&units=metric`;
-    const feelings = document.getElementById("feelings").value;
-    try{
-        const result = await fetch(url);
-        const data = await result.json();
-        const temp = data.main.temp;
-        await fetch('/saveData', {
+btn.addEventListener('click', action); //adding event listener with a call back function to perform action when user click on generate button
+async function action() {                                     // Asyncrounus function 
+    const zip = document.querySelector('#zip').value;        // getting the value of zip code user input 
+    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${key}&units=metric`;  // API url included with zip and key 
+    const feelings = document.getElementById("feelings").value;    // getting the value of feeling  user input
+    try{                                                          // to handle  code errors 
+        const result = await fetch(url);                         // using fetch to get the data fro api service 
+        const data = await result.json();                      // converting the data coming from api server to JS object by json 
+        const temp = data.main.temp;                           // access the object to get the requested temp. only  
+        await fetch('/saveData', {                             // fetching the received data from api to local server using the method POSt 
             method: "POST",
             credentials: 'same-origin',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
+            body: JSON.stringify({                           // converting the data to save it in our global vrialble dataobject in the local server
                 date: newDate,
                 temp: temp,
                 feelings: feelings
             })
-        });
-        const res = await fetch('/getData')
-        const final = await res.json();
-        console.log(final.temp);
-        let date = document.getElementById("date");
-        let temprature = document.getElementById("temp");
-        let content = document.getElementById("content");
-        date.innerText = `Date : ${final.date}`
-        temprature.innerText = `temprature : ${final.temp}`
-        content.innerText = `your feeling is ${final.feelings}`
-
+            
+        }).then(updateUI())               //promise function to updae the user UI and show final results
+        ;       
     }
     catch(err){console.log(err)}
 };
 
-// let updateUI = document.getElementById("feelings");
-// updateUI.innerText = final.feelings
-// updateUI.innerText = final.date
-// updateUI.innerText = final.temp
 
+const updateUI = async () => {
+
+    const request = await fetch('/getData');
+    try {
+        const allData = await request.json();
+        document.getElementById('date').innerHTML = allData.date;
+        document.getElementById('temp').innerHTML = allData.temp;
+        document.getElementById('content').innerHTML = allData.feelings;
+    } catch (error) {
+        console.log("error", error);
+    }
+}
